c671d07da6b99b501aa0a95dbb23ebe25663293c
Ava Williams <avawilliams@Avas-Air.hsd1.ga.comcast.net>
2025-05-12 01:22:52 -0400

    basic command line use

--- caesar_shift.py
+++ caesar_shift.py
@@ -43,11 +43,20 @@ def decrypt_unknown_shift(message):
     return potential_messages
 
 def head():
-    mode = input("Encrypt or Decrypt? ")
-    if (mode == "Encrypt"):
-        print("Encrypted: ", encrypt())
-    elif (mode == "Decrypt"):
-        print("Decrypted: ", decrypt())
+    mode = input("Encrypt or Decrypt? ").strip().lower()
+    if (mode == "encrypt"):
+        message = input("What would you like to encrypt? ")
+        shift = int(input("What is the desired shift? "))
+        print("Encrypted: ", encrypt(message, shift))
+    elif (mode == "decrypt"):
+        message = input("What would you like to decrypt? ")
+        know_shift = input("Do you know the key? Type Y/N: ").strip().lower()
+        if (know_shift == "y"):
+            shift = int(input("What is the key? "))
+            print("Decrypted: ", decrypt_shift(message, shift))
+        else: 
+            decrypted = decrypt_unknown_shift(message)
+            print("Here are some potential plaintexts:")
+            for d in decrypted:
+                print(d)
     return
-
-# head()
