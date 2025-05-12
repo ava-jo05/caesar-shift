9883dcd3767eae45bfc6e02f39255e439a0a3b55
Ava Williams <avawilliams@Avas-Air.hsd1.ga.comcast.net>
2025-05-12 01:05:04 -0400

    Decrypt and Encrypt Functional

--- caesar_shift.py
+++ caesar_shift.py
@@ -1,34 +1,46 @@
 import string
 
-def encrypt():
-    message = (input("What's the secret message? ")).upper()
-    shift = int(input("What's the desired shift? ")) % 26
+def encrypt(message, shift):
+    message = message.upper()
+    shift = shift % 26
     new_message = ""
     for m in message:
-        if (m != string.ascii_letters): pass
-        new_message += chr((ord(m) - 65 + shift) % 26 + 65)
+        if (not (m in string.ascii_letters)):
+            new_message += m
+        else:
+            new_message += chr((ord(m) - 65 + shift) % 26 + 65)
     return new_message
 
-def find_common_letter(text):
-    freq = {}
-    for char in text:
-        freq[char] = freq.get(char, 0) + 1
-    max_char = max(freq, key=freq.get)
-    shift = ord("E") - ord(max_char) 
-    return shift * -1 if shift < 0 else shift
-
-def decrypt():
-    cipher = (input("What do you want to decrypt? ")).upper()
-    shift = input("Do you know the shift? Type Y/N: ")
-    if (shift == "Y"):
-        shift = int(input("What's the desired shift? ")) % 26
-    else: 
-        shift = find_common_letter(cipher)
+def decrypt_shift(message, shift):
+    message = message.upper()
+    shift = shift % 26
     original_message = ""
-    for c in cipher:
-        if (c != string.ascii_letters): pass
-        original_message += chr((ord(c) - 65 - shift) % 26 + 65)
+    for c in message:
+        if (not (c in string.ascii_letters)):
+            original_message += c
+        else:
+            original_message += chr((ord(c) - 65 - shift) % 26 + 65)
     return original_message
+
+def find_shift(text):
+    freq = {}
+    shift_set = set()
+    most_common_letters = ["E", "T", "A"]
+    for char in text:
+            freq[char] = freq.get(char, 0) + 1
+    max_char = max(freq, key=freq.get)
+    for i in range(3):
+        shift = ord(most_common_letters[i]) - ord(max_char)
+        shift_set.add(shift * -1 if shift < 0 else shift)
+    return shift_set
+
+def decrypt_unknown_shift(message):
+    message = message.upper()
+    potential_shifts = find_shift(message)
+    potential_messages = []
+    for shift in potential_shifts:
+        potential_messages.append(decrypt_shift(message, shift))
+    return potential_messages
 
 def head():
     mode = input("Encrypt or Decrypt? ")
@@ -38,4 +50,4 @@ def head():
         print("Decrypted: ", decrypt())
     return
 
-head()
+# head()
--- test_caesar_shift.py
+++ test_caesar_shift.py
@@ -1,6 +1,42 @@
 import unittest
+from caesar_shift import decrypt_shift, decrypt_unknown_shift, encrypt, find_shift
 
 class TestCaesarShift(unittest.TestCase):
+    def test_encrypt_1(self):
+        self.assertEqual(encrypt("ABC", 3), 'DEF')
+    def test_encrypt_2(self):
+        self.assertEqual(encrypt("XYZ", 26), "XYZ")
+    def test_encrypt_3(self):
+        self.assertEqual(encrypt("hello", 29), "KHOOR")
+    def test_encrypt_4(self):
+        self.assertEqual(encrypt("Hello, World!", 7), "OLSSV, DVYSK!")
+
+    def test_decrypt_shift_1(self):
+        self.assertEqual(decrypt_shift("DEF", 3), 'ABC')
+    def test_decrypt_shift_2(self):
+        self.assertEqual(decrypt_shift("XYZ", 26), "XYZ") 
+    def test_decrypt_shift_3(self):
+        self.assertEqual(decrypt_shift("khoor", 29), "HELLO")  
+    def test_decrypt_shift_4(self):
+        self.assertEqual(decrypt_shift("OLSSV, DVYSK!", 7), "HELLO, WORLD!")
+    
+    def test_find_shift_1(self):
+        self.assertEqual(find_shift("RHCPC"), {2, 17}) # THERE, 2
+    def test_find_shift_2(self):
+        self.assertEqual(find_shift("YNYFSNH"), {24, 5, 20}) # TITANIC, 5
+    def test_find_shift_3(self):
+        self.assertEqual(find_shift("NNEQINEX"), {9, 6, 13}) # AARDVARK, 13
+    def test_find_shift_4(self):
+        self.assertEqual(find_shift("WHD WLPH!"), {18, 3, 22}) # TEA TIME!, 3
+    
+    def test_decrypt_unknown_shift_1(self):
+        self.assertEqual(decrypt_unknown_shift("PANNW"), ["GREEN", "CNAAJ", "JUHHQ"])
+    def test_decrypt_unknown_shift_2(self):
+        self.assertEqual(decrypt_unknown_shift("RUBK RKZZKX"), ["ILSB IBQQBO", "HKRA HAPPAN", "LOVE LETTER"])
+    def test_decrypt_unknown_shift_3(self):
+        self.assertEqual(decrypt_unknown_shift("NBDCPPL"), ["CQSREEA", "JXZYLLH", "YMONAAW"])
+    def test_decrypt_unknown_shift_4(self):
+        self.assertEqual(decrypt_unknown_shift("PEEATHPJRT"), ["ETTPIWEYGI", "LAAWPDLFNP", "APPLESAUCE"])
 
 if __name__ == '__main__':
     unittest.main()
